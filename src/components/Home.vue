a8d24690df23b11452d5559d89a102c6ba4065e4
beardedpayton <beardedpayton@gmail.com>
2018-03-09 10:00:00 -0500

    some logic and data added to home/summary, need to make details into its own

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,17 @@
 <template>
   <section>
-    <div>
-      <h1>Trending Anime</h1>
-      <v-layout row wrap>
-        <v-flex xs3 v-for="anime in trending" :key="anime.id">
-          <router-link :to="{ name: 'anime-detail', params: { id: anime.id }}">
-            <div class="title-wrapper">
-              <img :src="anime.attributes.posterImage.medium" :alt="anime.attributes.canonicalTitle">
-              <h4>{{ anime.attributes.canonicalTitle }}</h4>
-              <span>Start Date: {{ anime.attributes.startDate}}</span>
-            </div>
-          </router-link>
-        </v-flex>
-      </v-layout>
-    </div>
+    <h1>Trending Anime</h1>
+    <v-layout row wrap>
+      <v-flex md3 v-for="anime in trending" :key="anime.id">
+        <router-link :to="{ name: 'anime-detail', params: { id: anime.id }}">
+          <div class="title-wrapper">
+            <img :src="anime.attributes.posterImage.medium" :alt="anime.attributes.canonicalTitle">
+            <h4>{{ anime.attributes.canonicalTitle }}</h4>
+            <span>Start Date: {{ anime.attributes.startDate}}</span>
+          </div>
+        </router-link>
+      </v-flex>
+    </v-layout>
   </section>
 </template>
 
--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -11,16 +11,22 @@
     <v-flex md4 sm6>
       <div class="synopsis">
         <h2>{{ summaryData.canonicalTitle }}</h2>
-        <p>{{ summaryData.synopsis }}</p>
+        <p class="short-synopsis" v-if="summaryData.synopsis.length >= 500">
+          {{summaryData.synopsis.substring(0,500) + '...'}}
+          <br><a @click="displayMore">Read More</a>
+        </p>
+        <p class="full-synopsis" v-else>{{ summaryData.synopsis }}</p>
+        <p class="hidden-on-load">{{ summaryData.synopsis }}</p>
       </div>
     </v-flex>
     <v-flex md3>
       <div class="details">
-        <h4>Anime Details</h4>
+        <h3>Anime Details</h3>
         <ul>
-          <li>Type</li>
-          <li>Episodes</li>
-          <li>Status</li>
+          <li><strong>English</strong><br>{{summaryData.titles.en}}</li>
+          <li><strong>Romanized</strong><br>{{summaryData.titles.en_jp}}</li>
+          <li><strong>Japanese</strong><br>{{summaryData.titles.ja_jp}}</li>
+          <li><strong>Synonyms</strong><br><span v-for="title in summaryData.abbreviatedTitles" :key="title">{{title}}</span></li>
         </ul>
       </div>
     </v-flex>
@@ -30,7 +36,13 @@
 <script>
 export default {
   name: 'Summary',
-  props: ['summaryData']
+  props: ['summaryData'],
+  methods: {
+    displayMore () {
+      document.querySelector('.short-synopsis').classList.add('hidden')
+      document.querySelector('.hidden-on-load').classList.add('show')
+    }
+  }
 }
 </script>
 
@@ -48,6 +60,7 @@ export default {
 .details {
   width: 290px;
   height: 500px;
+  font-size: 14px;
   padding: 20px;
   margin: 20px 0 20px 35px;
   background-color: #fff;
@@ -57,9 +70,22 @@ export default {
 .details ul {
   list-style-type: none;
 }
+.details li {
+  margin-bottom: 10px;
+}
+.details h3 {
+  margin-bottom: 15px;
+}
 @media (max-width: 599px) {
   .poster-img {
     display: none;
   }
+}
+.hidden,
+.hidden-on-load {
+  display: none;
+}
+.show {
+  display: block;
 }
 </style>
